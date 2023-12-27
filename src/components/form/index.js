4bef9622a38aaef5ff2606f7de40d20a4a52c3ca
BeluKotu <vietthanh456@gmail.com>
2023-12-28 03:33:41 +0700

    absorbingForm

---
+++ src/components/form/index.js
@@ -0,0 +1,3 @@
+export { default as FCheckBox } from "./components/form/FCheckBox";
+export { default as FormProvider } from "./components/form/FormProvider";
+export { default as FTextField } from "./components/form/FTextField";
--- src/pages/LogInForm.js
+++ src/pages/LogInForm.js
@@ -16,6 +16,9 @@ import LoadingButton from "@mui/lab/LoadingButton";
 import { useForm, Controller } from "react-hook-form";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import FormProvider from "../components/form/FormProvider";
+import FTextField from "../components/form/FTextField";
+import FCheckBox from "../components/form/FCheckBox";
 
 function LogInForm() {
   const defaultValues = {
@@ -47,85 +50,41 @@ function LogInForm() {
       <Typography variant="h3" textAlign="center" mb={3}>
         Log In
       </Typography>
-      <form onSubmit={handleSubmit(onSubmit)}>
+
+      <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
         <Stack spacing={2}>
           {!!errors.afterSubmit && (
             <Alert severity="error">{errors.afterSubmit.message}</Alert>
           )}
-          <Controller
-            name="email"
-            control={control}
-            render={({ field, fieldState: { error } }) => (
-              <TextField
-                label="Email Address"
-                // type="email"
-                // {...register("email", {
-                //   required: "required",
-                //   pattern: {
-                //     value: /\S+@\S+\.\S+/,
-                //     message: "Entered value does not match email format",
-                //   },
-                // })}
-                fullWidth
-                error={!!error}
-                helperText={error?.message}
-                {...field}
-              />
-            )}
-          />
-          <Controller
+          <FTextField name="email" label="email address" />
+
+          <FTextField
             name="password"
-            control={control}
-            render={({ field, fieldState: { error } }) => (
-              <TextField
-                label="password"
-                type={showPassword ? "text" : "password"}
-                fullWidth
-                error={!!error}
-                helperText={error?.message}
-                {...field}
-                // {...register("password", {
-                //   required: "required",
-                //   minLength: {
-                //     value: 5,
-                //     message: "min length is 5",
-                //   },
-                // })}
-                // TODO: Add toggle hide/show password here from MUI
-                InputProps={{
-                  endAdornment: (
-                    <InputAdornment position="end">
-                      <IconButton
-                        aria-label="toggle password visibility"
-                        onClick={handleClickShowPassword}
-                        onMouseDown={handleMouseDownPassword}
-                      >
-                        {showPassword ? <Visibility /> : <VisibilityOff />}
-                      </IconButton>
-                    </InputAdornment>
-                  ),
-                }}
-              />
-            )}
+            label="password"
+            type={showPassword ? "text" : "password"}
+            fullWidth
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <IconButton
+                    aria-label="toggle password visibility"
+                    onClick={handleClickShowPassword}
+                    onMouseDown={handleMouseDownPassword}
+                  >
+                    {showPassword ? <Visibility /> : <VisibilityOff />}
+                  </IconButton>
+                </InputAdornment>
+              ),
+            }}
           />
+
           <Stack
             direction="row"
             alignItems="center"
             justifyContent="space-between"
             sx={{ my: 2 }}
           >
-            <FormControlLabel
-              label="remember my ass"
-              control={
-                <Controller
-                  name="remember"
-                  control={control}
-                  render={({ field }) => (
-                    <Checkbox {...field} checked={field.value} />
-                  )}
-                />
-              }
-            />{" "}
+            <FCheckBox name="remember" label="remember my ass" />
           </Stack>
           <LoadingButton
             fullWidth
@@ -138,9 +97,26 @@ function LogInForm() {
             Log In
           </LoadingButton>
         </Stack>
-      </form>
+      </FormProvider>
     </Box>
   );
 }
 
 export default LogInForm;
+
+// type="email"
+// {...register("email", {
+//   required: "required",
+//   pattern: {
+//     value: /\S+@\S+\.\S+/,
+//     message: "Entered value does not match email format",
+//   },
+// })}
+
+// {...register("password", {
+//   required: "required",
+//   minLength: {
+//     value: 5,
+//     message: "min length is 5",
+//   },
+// })}
