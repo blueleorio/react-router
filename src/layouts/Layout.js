d75662f1594264867d43080c65c773cb9704bc66
BeluKotu <vietthanh456@gmail.com>
2024-01-05 15:58:03 +0700

    randomCommit

---
+++ src/layouts/Layout.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+import Navigation from "../components/Navigation";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+
+export const Layout = () => {
+  return (
+    <Stack
+      sx={{
+        minHeight: "100vh",
+        backgroundColor: (theme) => theme.palette.primary.main,
+      }}
+    >
+      <Navigation />
+      <Outlet />
+      <Box sx={{ flexGrow: 1 }} />
+    </Stack>
+  );
+};
