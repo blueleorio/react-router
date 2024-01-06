07093e05c8a6e714eaa9f6bd336bb728addbf5d9
BeluKotu <vietthanh456@gmail.com>
2024-01-07 00:52:09 +0700

    updateAuthContextAllInOne

--- src/auth/AuthContext.js
+++ src/auth/AuthContext.js
@@ -1,4 +1,43 @@
-import React from "react";
+import React, { createContext, useContext, useState } from "react";
+import { fakeAuthProvider } from "./auth";
+import { useLocation } from "react-router-dom";
 
-let AuthContext = React.createContext(null);
-export default AuthContext;
+const Auth = createContext(null);
+
+// I still dont know what this does! need to revisit UseContext tutorial
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const signin = (newUser, callback) => {
+    return fakeAuthProvider.signin(() => {
+      setUser(newUser);
+      callback();
+    });
+  };
+
+  const signout = (callback) => {
+    return fakeAuthProvider.signout(() => {
+      setUser(null);
+      callback();
+    });
+  };
+
+  const value = { user, signin, signout };
+
+  return <Auth.Provider value={value}>{children}</Auth.Provider>;
+};
+
+// UseAuth - to call the context
+const useAuth = () => {
+  return useContext(Auth);
+};
+
+// Wrapper wateva this is
+const RequireAuth = ({ children }) => {
+  const auth = useAuth();
+  const location = useLocation();
+  // Use auth and location as needed
+  return children;
+};
+
+export { AuthProvider, useAuth, RequireAuth };
